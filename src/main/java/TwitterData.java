26743b8bc89f59800affe5ef1faa8850370e6860
Christopher Chevalier <chevalierc@wit.edu>
2015-04-06 13:49:30 -0400

    Merge branch 'master' of github.com:doomboss/COMP310project
    Conflicts:
    	src/main/java/TwitterDataMiner.java

---
+++ src/main/java/TwitterData.java
@@ -0,0 +1,19 @@
+import twitter4j.GeoLocation;
+import twitter4j.Query;
+import twitter4j.QueryResult;
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.conf.ConfigurationBuilder;
+
+
+public class TwitterData {
+	private int opinion;
+	private GeoLocation location;
+	private String tweet;
+	
+	//TODO: create constructor, handler, etc...
+	
+
+}
